798998da3dc9909ace7a410c870e867ced489ad8
farismoh <farismoh@uab.edu>
2016-05-09 15:34:48 -0500

    Fuel Economy Calculate Project

---
+++ Fuel Economy/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Fuel_Economy
+{
+    partial class fuelEconomy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exitButton = new System.Windows.Forms.Button();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.gallonsTextBox = new System.Windows.Forms.TextBox();
+            this.milesTextBox = new System.Windows.Forms.TextBox();
+            this.milesPromptLabel = new System.Windows.Forms.Label();
+            this.gallonsPromptLabel = new System.Windows.Forms.Label();
+            this.outputDescriptionLabel = new System.Windows.Forms.Label();
+            this.mpgLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(685, 520);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(229, 86);
+            this.exitButton.TabIndex = 0;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(179, 520);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(229, 86);
+            this.calculateButton.TabIndex = 1;
+            this.calculateButton.Text = "Calculate MPG";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // gallonsTextBox
+            // 
+            this.gallonsTextBox.Location = new System.Drawing.Point(685, 217);
+            this.gallonsTextBox.Name = "gallonsTextBox";
+            this.gallonsTextBox.Size = new System.Drawing.Size(177, 31);
+            this.gallonsTextBox.TabIndex = 2;
+            this.gallonsTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // milesTextBox
+            // 
+            this.milesTextBox.Location = new System.Drawing.Point(685, 143);
+            this.milesTextBox.Name = "milesTextBox";
+            this.milesTextBox.Size = new System.Drawing.Size(177, 31);
+            this.milesTextBox.TabIndex = 3;
+            this.milesTextBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // milesPromptLabel
+            // 
+            this.milesPromptLabel.AutoSize = true;
+            this.milesPromptLabel.Location = new System.Drawing.Point(237, 143);
+            this.milesPromptLabel.Name = "milesPromptLabel";
+            this.milesPromptLabel.Size = new System.Drawing.Size(328, 25);
+            this.milesPromptLabel.TabIndex = 4;
+            this.milesPromptLabel.Text = "Enter the numner of miles driven:";
+            // 
+            // gallonsPromptLabel
+            // 
+            this.gallonsPromptLabel.AutoSize = true;
+            this.gallonsPromptLabel.Location = new System.Drawing.Point(237, 223);
+            this.gallonsPromptLabel.Name = "gallonsPromptLabel";
+            this.gallonsPromptLabel.Size = new System.Drawing.Size(298, 25);
+            this.gallonsPromptLabel.TabIndex = 5;
+            this.gallonsPromptLabel.Text = "Enter the gallons of gas used:";
+            // 
+            // outputDescriptionLabel
+            // 
+            this.outputDescriptionLabel.AutoSize = true;
+            this.outputDescriptionLabel.Location = new System.Drawing.Point(237, 326);
+            this.outputDescriptionLabel.Name = "outputDescriptionLabel";
+            this.outputDescriptionLabel.Size = new System.Drawing.Size(163, 25);
+            this.outputDescriptionLabel.TabIndex = 6;
+            this.outputDescriptionLabel.Text = "Your car\'s MPG";
+            // 
+            // mpgLabel
+            // 
+            this.mpgLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.mpgLabel.Location = new System.Drawing.Point(685, 307);
+            this.mpgLabel.Name = "mpgLabel";
+            this.mpgLabel.Size = new System.Drawing.Size(177, 42);
+            this.mpgLabel.TabIndex = 7;
+            this.mpgLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // fuelEconomy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1047, 698);
+            this.Controls.Add(this.mpgLabel);
+            this.Controls.Add(this.outputDescriptionLabel);
+            this.Controls.Add(this.gallonsPromptLabel);
+            this.Controls.Add(this.milesPromptLabel);
+            this.Controls.Add(this.milesTextBox);
+            this.Controls.Add(this.gallonsTextBox);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.exitButton);
+            this.Name = "fuelEconomy";
+            this.Text = "Fuel Economy";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.TextBox gallonsTextBox;
+        private System.Windows.Forms.TextBox milesTextBox;
+        private System.Windows.Forms.Label milesPromptLabel;
+        private System.Windows.Forms.Label gallonsPromptLabel;
+        private System.Windows.Forms.Label outputDescriptionLabel;
+        private System.Windows.Forms.Label mpgLabel;
+    }
+}
+
---
+++ Fuel Economy/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fuel_Economy
+{
+    public partial class fuelEconomy : Form
+    {
+        public fuelEconomy()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            double miles;       // To hold miles driven
+            double gallons;     // To hold gallons used
+            double mpg;         // To hold MPG
+
+            //Get the miles driven and assign it to 
+            //the miles variable. 
+            miles = double.Parse(milesTextBox.Text); 
+
+            //Get the gallons used and assign it to 
+            //the gallons variable
+            gallons = double.Parse(gallonsTextBox.Text); 
+
+            // Calculate MPG
+            mpg = miles / gallons; 
+
+            // Display the MPG in the mpgLabel control
+            mpgLabel.Text = mpg.ToString(); 
+
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
